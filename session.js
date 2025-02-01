7a7816193069c65afed44995d3b5218acf4371fe
wattoowaqar26 <wattoowaqar26@gmail.com>
2025-02-01 07:51:41 +0500

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~iBkFnZDY#5JhIWskfpoFZN2Ll2MQiN1CfQqR7sFNdq2IOwX3Zz50",
 PORT: process.env.PORT || "8000"
 };
